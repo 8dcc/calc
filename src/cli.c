bb523b0275c5aa6884c2924e04907f05dd8fdcd5
8dcc <8dcc.git@gmail.com>
2023-09-18 14:54:10 +0200

    Add input and simple CLI parsing
    Add main loop
    Add prompt
    Add colors to output

--- src/cli.c
+++ src/cli.c
@@ -1,8 +1,12 @@
 
 #include <stdint.h>
+#include <stdbool.h>
 #include <inttypes.h> /* PRId32, PRId64, etc. */
 #include <stdio.h>
 #include <stdlib.h>
+
+#define USE_COLOR
+#include "include/color.h"
 #include "include/cli.h"
 
 /* Size of the stack in elements, not bytes */
@@ -23,8 +27,34 @@ static inline int64_t stack_pop(void) {
 }
 
 static inline void stack_print(void) {
+    /* For COL_* defines, see include/color.h, for PRI* defines, see
+     * <inttypes.h> */
     for (uint32_t i = 0; i < stk_top; i++)
-        printf("[stk+%02" PRIu32 "] %" PRId64 "\n", i, stk[i]);
+        printf(COL_STK_PRE "[stk+%02" PRIu32 "]" COL_STK_NUM
+                           " %" PRId64 COL_NORM "\n",
+               i, stk[i]);
+}
+
+static uint32_t input_read(char* out, size_t bufsz) {
+    uint32_t ret = INPUT_INT;
+
+    uint32_t i;
+    for (i = 0; i < bufsz - 1; i++) {
+        char c = getchar();
+        if (c == '\n' || c == EOF)
+            break;
+
+        /* Non-number char? */
+        if (c == '.' && ret == INPUT_INT)
+            ret = INPUT_FLOAT;
+        else if (c < '0' || c > '9')
+            ret = INPUT_STR;
+
+        out[i] = c;
+    }
+
+    out[i] = '\0';
+    return (i == 0) ? INPUT_EMPTY : ret;
 }
 
 /*----------------------------------------------------------------------------*/
@@ -34,15 +64,35 @@ int cli_main(void) {
     stk     = calloc(STK_SZ, sizeof(int64_t));
     stk_top = 0;
 
-    /* Tests */
-    stack_push(123);
-    stack_push(555);
-    stack_push(444);
-    stack_print();
+    /* Input buffer */
+    char input_buf[INPUT_BUF_SZ] = { '\0' };
 
-    printf("Popped: %ld\n", stack_pop());
-    printf("Popped: %ld\n", stack_pop());
-    stack_print();
+    /* Main loop */
+    while (true) {
+        printf(COL_PROMPT "[calc]: " COL_NORM);
+        uint32_t input_code = input_read(input_buf, INPUT_BUF_SZ);
+
+        switch (input_code) {
+            case INPUT_STR:
+                /* TODO: Handle operations in separate func. */
+                if (input_buf[0] == 'q')
+                    return 0;
+
+                break;
+            case INPUT_INT:
+                stack_push(atoi(input_buf));
+                break;
+            case INPUT_FLOAT:
+                printf(COL_ERROR "Floats are not supported yet." COL_NORM "\n");
+                input_code = INPUT_EMPTY;
+                break;
+            default:
+                break;
+        }
+
+        if (input_code != INPUT_EMPTY)
+            stack_print();
+    }
 
     return 0;
 }
--- src/include/cli.h
+++ src/include/cli.h
@@ -1,6 +1,15 @@
 #ifndef CLI_H_
 #define CLI_H_ 1
 
+#define INPUT_BUF_SZ 255
+
+enum input_types {
+    INPUT_EMPTY = 0,
+    INPUT_STR   = 1,
+    INPUT_INT   = 2,
+    INPUT_FLOAT = 3,
+};
+
 int cli_main(void);
 
 #endif /* CLI_H_ */
