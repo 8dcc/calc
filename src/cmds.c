9e351b0c10ccceb0e7b77908e810270433d32d09
8dcc <8dcc.git@gmail.com>
2023-09-18 18:03:31 +0200

    Fix cmd_sub and cmd_div

--- src/cmds.c
+++ src/cmds.c
@@ -12,7 +12,7 @@ void cmd_add(void) {
 void cmd_sub(void) {
     const int64_t n1 = stack_pop();
     const int64_t n2 = stack_pop();
-    stack_push(n1 - n2);
+    stack_push(n2 - n1);
 }
 
 void cmd_mul(void) {
@@ -24,5 +24,5 @@ void cmd_mul(void) {
 void cmd_div(void) {
     const int64_t n1 = stack_pop();
     const int64_t n2 = stack_pop();
-    stack_push(n1 / n2);
+    stack_push(n2 / n1);
 }
