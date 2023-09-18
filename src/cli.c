09bc4932e311b607e64d831c51f95b6cf09acf1b
8dcc <8dcc.git@gmail.com>
2023-09-18 18:01:22 +0200

    Parse command strings in cli.c
    Move stack functions to stack.h
    Rename INPUT_* macros

--- src/cli.c
+++ src/cli.c
@@ -4,27 +4,24 @@
 #include <inttypes.h> /* PRId32, PRId64, etc. */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define USE_COLOR
 #include "include/color.h"
 #include "include/cli.h"
+#include "include/stack.h"
+#include "include/cmds.h"
 
-/* Size of the stack in elements, not bytes */
-#define STK_SZ 100
+#define LENGTH(arr) (int)(sizeof(arr) / sizeof((arr)[0]))
 
-/* Stack and stack position of stack top (element on top of the last one) */
-int64_t* stk;
-uint32_t stk_top;
+static Command cmds[] = {
+    { "+", cmd_add },
+    { "-", cmd_sub },
+    { "*", cmd_mul },
+    { "/", cmd_div },
+};
 
 /*----------------------------------------------------------------------------*/
-
-static inline void stack_push(int64_t n) {
-    stk[stk_top++] = n;
-}
-
-static inline int64_t stack_pop(void) {
-    return stk[--stk_top];
-}
 
 static inline void stack_print(void) {
     /* For COL_* defines, see include/color.h, for PRI* defines, see
@@ -36,7 +33,7 @@ static inline void stack_print(void) {
 }
 
 static uint32_t input_read(char* out, size_t bufsz) {
-    uint32_t ret = INPUT_INT;
+    uint32_t ret = INPUT_READ_INT;
 
     uint32_t i;
     for (i = 0; i < bufsz - 1; i++) {
@@ -45,52 +42,61 @@ static uint32_t input_read(char* out, size_t bufsz) {
             break;
 
         /* Non-number char? */
-        if (c == '.' && ret == INPUT_INT)
-            ret = INPUT_FLOAT;
+        if (c == '.' && ret == INPUT_READ_INT)
+            ret = INPUT_READ_FLOAT;
         else if (c < '0' || c > '9')
-            ret = INPUT_STR;
+            ret = INPUT_READ_STR;
 
         out[i] = c;
     }
 
     out[i] = '\0';
-    return (i == 0) ? INPUT_EMPTY : ret;
+    return (i == 0) ? INPUT_READ_EMPTY : ret;
+}
+
+static uint32_t input_parse_str(const char* s) {
+    /* Special commands */
+    if (!strcmp(s, "q") || !strcmp(s, "quit"))
+        return INPUT_PARSE_QUIT;
+
+    /* Stack commands */
+    for (size_t i = 0; i < LENGTH(cmds); i++)
+        if (!strcmp(s, cmds[i].cmd))
+            cmds[i].func();
+
+    return INPUT_PARSE_OK;
 }
 
 /*----------------------------------------------------------------------------*/
 
 int cli_main(void) {
-    /* Initialize stack and stack top */
-    stk     = calloc(STK_SZ, sizeof(int64_t));
-    stk_top = 0;
-
     /* Input buffer */
     char input_buf[INPUT_BUF_SZ] = { '\0' };
 
     /* Main loop */
     while (true) {
         printf(COL_PROMPT "[calc]: " COL_NORM);
-        uint32_t input_code = input_read(input_buf, INPUT_BUF_SZ);
+        uint32_t input_read_code = input_read(input_buf, INPUT_BUF_SZ);
 
-        switch (input_code) {
-            case INPUT_STR:
-                /* TODO: Handle operations in separate func. */
-                if (input_buf[0] == 'q')
+        switch (input_read_code) {
+            case INPUT_READ_STR:
+                uint32_t input_parse_code = input_parse_str(input_buf);
+                if (input_parse_code == INPUT_PARSE_QUIT)
                     return 0;
 
                 break;
-            case INPUT_INT:
+            case INPUT_READ_INT:
                 stack_push(atoi(input_buf));
                 break;
-            case INPUT_FLOAT:
+            case INPUT_READ_FLOAT:
                 printf(COL_ERROR "Floats are not supported yet." COL_NORM "\n");
-                input_code = INPUT_EMPTY;
+                input_read_code = INPUT_READ_EMPTY;
                 break;
             default:
                 break;
         }
 
-        if (input_code != INPUT_EMPTY)
+        if (input_read_code != INPUT_READ_EMPTY)
             stack_print();
     }
 
--- src/include/cli.h
+++ src/include/cli.h
@@ -3,11 +3,17 @@
 
 #define INPUT_BUF_SZ 255
 
-enum input_types {
-    INPUT_EMPTY = 0,
-    INPUT_STR   = 1,
-    INPUT_INT   = 2,
-    INPUT_FLOAT = 3,
+enum input_read_types {
+    INPUT_READ_EMPTY = 0,
+    INPUT_READ_STR   = 1,
+    INPUT_READ_INT   = 2,
+    INPUT_READ_FLOAT = 3,
+};
+
+enum input_parse_types {
+    INPUT_PARSE_ERROR = 0,
+    INPUT_PARSE_OK    = 1,
+    INPUT_PARSE_QUIT  = 2,
 };
 
 int cli_main(void);
