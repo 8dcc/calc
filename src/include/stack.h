babfb6fbfe816fdca1444b3e6d864b167e0315cf
8dcc <8dcc.git@gmail.com>
2023-09-21 14:24:44 +0200

    Add stack boundary checks when popping and pushing

--- src/include/stack.h
+++ src/include/stack.h
@@ -1,6 +1,8 @@
 #ifndef STACK_H_
 #define STACK_H_ 1
 
+#include "util.h"
+
 /* Size of the stack in elements, not bytes */
 #define STK_SZ 100
 
@@ -9,11 +11,18 @@ extern int64_t* stk;
 extern uint32_t stk_top;
 
 static inline void stack_push(int64_t n) {
-    stk[stk_top++] = n;
+    if (stk_top < STK_SZ)
+        stk[stk_top++] = n;
+    else
+        err_msg("stack_push: Reached top of stack (%d)", STK_SZ);
 }
 
 static inline int64_t stack_pop(void) {
-    return stk[--stk_top];
+    if (stk_top < STK_SZ)
+        return stk[--stk_top];
+
+    err_msg("stack_pop: Reached bottom of stack");
+    return 0;
 }
 
 #endif /* STACK_H_ */
